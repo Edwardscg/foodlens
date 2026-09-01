d1e7686ef4c9d4c91619b0c83aae4fa4efb90fa8
Edward <edwardcornegomez@gmail.com>
2026-09-01 00:13:08 -0500

    feat(backend): initialize FoodLens backend

---
+++ foodlens-backend/src/main/java/com/foolens/backend/FoodlensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foolens.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlensBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodlensBackendApplication.class, args);
+    }
+
+}
---
+++ foodlens-backend/src/test/java/com/foolens/backend/FoodlensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foolens.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlensBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
